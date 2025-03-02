dbc455f6347a9bd7d0bf754221dea16860bc8bda
Mirkhat Assen <asen.mirkhat@bk.ru>
2025-03-02 20:11:15 +0500

    init

---
+++ src/app/common-ui/offer-product-card/offer-product-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-product-card',
+  imports: [],
+  templateUrl: './offer-product-card.component.html',
+  standalone: true,
+  styleUrl: './offer-product-card.component.scss'
+})
+export class OfferProductCardComponent {
+
+}
